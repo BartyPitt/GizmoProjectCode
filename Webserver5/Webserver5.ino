248508ca2887be0a809a63cd7bf674daa0a02af1
Barty Pitt <bartypitt@gmail.com>
2019-02-24 19:52:52 +0000

    Update Webserver5.ino

--- Webserver5/Webserver5.ino
+++ Webserver5/Webserver5.ino
@@ -1,26 +1,7 @@
-/*------------------------------------------------------------------------------
-  07/01/2018
-  Author: Makerbro
-  Platforms: ESP8266
-  Language: C++/Arduino
-  File: webserver_html_js.ino
-  ------------------------------------------------------------------------------
-  Description: 
-  Code for YouTube video demonstrating how to use JavaScript in HTML weppages
-  that are served in a web server's response.
-  https://youtu.be/ZJoBy2c1dPk
-  Do you like my videos? You can support the channel:
-  https://patreon.com/acrobotic
-  https://paypal.me/acrobotic
-  ------------------------------------------------------------------------------
-  Please consider buying products from ACROBOTIC to help fund future
-  Open-Source projects like this! We'll always put our best effort in every
-  project, and release all our design files and code for you to use. 
-  https://acrobotic.com/
-  https://amazon.com/acrobotic
-  ------------------------------------------------------------------------------
-  License:
-  Please see attached LICENSE.txt file for details.
+/*
+ Barty Pitt
+ Attempt 5 ish 
+ Why does nothing work :(
 ------------------------------------------------------------------------------*/
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
